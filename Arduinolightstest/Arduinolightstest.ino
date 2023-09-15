ee75242e314d7a2f75d62f3c5c879268bb9e4440
qitb <liamjgallagher@icloud.com>
2023-09-14 20:11:05 -0400

    moved all things to functions and set it to run fire

--- Arduinolightstest/Arduinolightstest.ino
+++ Arduinolightstest/Arduinolightstest.ino
@@ -4,6 +4,7 @@
 #define NUMPIXELS 21
 #define DATAPIN 5
 #define CLOCKPIN 6
+#define WAIT 10000
 
 Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
 
@@ -14,14 +15,35 @@ void setup() {
  
 }
 
-void loop() {
-   //building purple
-  for (int b=0; b <=100; b+=20) {
+    void fireRandom(){
+        for (int p=1; p<=NUMPIXELS; p++) {
+          strip.setPixelColor(p,random(1,255),random(1,24),0);
+           }
+           strip.show();
+           delay(100);
+    }
+
+
+    
+
+    void waterRandom(){
+       for (int p=1; p<=NUMPIXELS; p++) {
+      strip.setPixelColor(p,0,random(1,255),random(1,255));
+       }
+       strip.show();
+       delay(100);
+        }
+
+
+
+        
+   void buildingPurple(){
+    for (int b=0; b <=100; b+=10) {
     for (int p=1; p<=NUMPIXELS; p++){
       strip.setPixelColor(p, b, 0, b);
       strip.show();
       delay(15);
-    }}
+    }
     for (int b=b; b >=1; b-=10) {
       for (int p=1; p<=NUMPIXELS; p++){
         strip.setPixelColor(p, b, 0, b);
@@ -29,18 +51,20 @@ void loop() {
         delay(15);
       }
     }
-    
-/*
-      for (int p=1; p<=NUMPIXELS; p++) {
-      strip.setPixelColor(p,random(1,255),0,random(1,255));
+   }
+   }
+ void pinkRandom(){
+  for (int p=1; p<=NUMPIXELS; p++) {
+      strip.setPixelColor(p,random(100,150),random(1,50),random(50,100));
        }
        strip.show();
        delay(100);
-    */
-    //red fade (broken)
-  /*for (int r=0; r <100; r+=5){
-    for (int p=1; p<=NUMPIXELS; p++){
-      strip.setPixelColor(p, r, 0,0);
+ }
+
+  void redFade(){
+    for (int r=0; r <100; r+=5){
+      for (int p=1; p<=NUMPIXELS; p++){
+        strip.setPixelColor(p, r, 0,0);
       
     }
   }
@@ -53,8 +77,24 @@ void loop() {
     }
     strip.show();
     delay(100);
+}
+}
    
-  }*/
+  void loop() {
+
+   fireRandom();
+   delay(WAIT);/*
+   waterRandom();
+   delay(WAIT);
+   pinkRandom();
+   delay(WAIT);
+   buildingPurple();
+   delay(WAIT);
+   redFade();
+   delay(WAIT);*/
+   }
+    
+  
 
 
 
@@ -66,4 +106,3 @@ void loop() {
 
 
     
-}
